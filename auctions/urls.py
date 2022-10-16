2828402936cd824556589ff5ad1d72e42ad9e737
A.Vardhanreddy <vardhanreddy1411@gmail.com>
2022-10-16 08:48:45 +0530

    commited

---
+++ auctions/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from . import views
+from django.views.static import serve
+from django.conf.urls import url
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('register', views.register, name='register'),
+    path('auction/create', views.auction_create, name='auction_create'),
+    path('auction/active', views.active_auctions_view, name='active_auctions_view'),
+    path('auction/active/<str:category_name>', views.active_auctions_view, name='active_auctions_view'),
+    path('auction/watchlist', views.watchlist_view, name='watchlist_view'),
+    path('auction/watchlist/<int:auction_id>/edit/<str:reverse_method>', views.watchlist_edit, name='watchlist_edit'),
+    path('auction/<str:auction_id>', views.auction_details_view, name='auction_details_view'),
+    path('auction/<str:auction_id>/bid', views.auction_bid, name='auction_bid'),
+    path('auction/<str:auction_id>/close', views.auction_close, name='auction_close'),
+    path('auction/<str:auction_id>/comment', views.auction_comment, name='auction_comment'),
+    path('categories/<str:category_name>', views.category_details_view, name='category_details_view'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]
